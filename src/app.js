80299088d6082a50b78905550ea12a7865386e08
Julian Rodriguez <112611772+julianfroes@users.noreply.github.com>
2023-11-12 16:07:14 -0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 
-import employeesRoutes from "./routes/employees.routes.js";
+import ventasRoutes from "./routes/venta.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/", indexRoutes);
-app.use("/api", employeesRoutes);
+app.use("/api", ventasRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
